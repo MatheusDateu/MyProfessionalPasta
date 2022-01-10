1d8c26155274b08bf9fa4a895e7e5d03ec153bc9
MatheusDateu <mdelmondes5@gmail.com>
2022-01-10 18:20:57 -0400

    Upload

--- AppWebUm/AppWebUm/Pages/site-simples/app.js
+++ AppWebUm/AppWebUm/Pages/site-simples/app.js
@@ -1,12 +1,12 @@
     const switcher = document.querySelector('.btn');
     switcher.addEventListener('click', function () {
-        document.body.classList.toggle('dark-theme');
-        var className = document.body.className;
+        document.body.classList.toggle('dark-theme');//it can turn the dark-theme on or off
+        var className = document.body.className;//a variable to inform the current theme of the body
         if (className == "light-theme") {
             this.textContent = "Dark";
         }
-        if (className == "dark-theme") {
+        else{
             this.textContent = "Light";
         }
-        console.log('current class name: ' + className);
+        console.log('current class name: ' + className);//inform what is the current theme to developer
     });
